924faed94bd8bb87eb9609bb885d7cfb5cdcff05
erikvw <ew2789@gmail.com>
2016-07-05 14:00:20 +0200

    update app

--- edc_content_type_map/apps.py
+++ edc_content_type_map/apps.py
@@ -6,12 +6,15 @@ from django.db.models.signals import post_migrate
 
 def edc_content_type_callback(sender, **kwargs):
     from edc_content_type_map.helpers import ContentTypeMapHelper
-    sys.stdout.write('Running post migration for {} ...\n'.format(sender.verbose_name))
-    sys.stdout.flush()
+    verbose = True if kwargs.get('verbose') is None else kwargs.get('verbose')
+    if verbose:
+        sys.stdout.write('Running post migration for {} ...\n'.format(sender.verbose_name))
+        sys.stdout.flush()
     ContentTypeMapHelper().populate()
     ContentTypeMapHelper().sync()
-    sys.stdout.write(' Done. {}.\n'.format(sender.verbose_name))
-    sys.stdout.flush()
+    if verbose:
+        sys.stdout.write(' Done. {}.\n'.format(sender.verbose_name))
+        sys.stdout.flush()
 
 
 class EdcContentTypeAppConfig(AppConfig):
